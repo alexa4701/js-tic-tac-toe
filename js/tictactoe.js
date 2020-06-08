267ebedce027dcc840995da21159e20a8afbc097
Alexa Coffin <alexacoffin1@gmail.com>
2020-06-07 19:00:06 -0500

    wip - added game logic

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -12,10 +12,6 @@
 
         let board = [];
 
-
-
-        // console.log(board);
-
         return {
             initBoard: function() {
                 board.push(space('top', 'left'));
@@ -31,6 +27,9 @@
             getBoard: function() {
                 return board;
             },
+            getSpace: function(index) {
+                return board[index];
+            },
             getRowByIndex: function(index) {
                 let row = board[index].row;
 
@@ -40,6 +39,49 @@
                 let column = board[index].column;
 
                 return board.filter(space => space.column === column);
+            },
+            getDiagonalCenter: function(num) {
+                let diagonal = [];
+
+                diagonal.push(board[4]);
+                if (num === 1) {
+                    diagonal.push(0);
+                    diagonal.push(8);
+                } else if (num === 2) {
+                    diagonal.push(2);
+                    diagonal.push(6);
+                }
+
+                return diagonal;
+            },
+            getDiagonalByIndex: function(index) {
+                let column = board[index].column;
+                let row = board[index].row;
+                let diagonal = [];
+
+                diagonal.push(board[index]);
+                switch (row) {
+                    case 'top':
+                        if(column === 'left') {
+                            diagonal.push(board[4]);
+                            diagonal.push(board[8]);
+                        } else if (column === 'right') {
+                            diagonal.push(board[4]);
+                            diagonal.push(board[6]);
+                        }
+                        break;
+                    case 'bottom':
+                        if(column === 'left') {
+                            diagonal.push(board[4]);
+                            diagonal.push(board[2]);
+                        } else if (column === 'right') {
+                            diagonal.push(board[4]);
+                            diagonal.push(board[0]);
+                        }
+                        break;
+                }
+
+                return diagonal;
             },
             updateSpace: function(index, symbol) {
                 if(!board[index].taken) {
@@ -58,17 +100,14 @@
             spaces: '.space'
         };
 
-        // 1. Board space is clicked
-        // 2. Put X or O on the space in DOM
-        // 3. Game -> GameBoard -> Set space.move to X or O
-        // 4. Game -> GameBoard -> Set space.taken to true
-        // 5. Game -> Check for win conditions
         function updateSpace(event) {
-            let element = event.target;
-            if(!element.textContent) {
-                let currentPlayer = Game.getCurrentPlayer();
-                element.textContent = currentPlayer.symbol;
-                Game.turn(event.target.getAttribute('data-index'));
+            if(!Game.getGameOverFlag()) {
+                let element = event.target;
+                if(!element.textContent) {
+                    let currentPlayer = Game.getCurrentPlayer();
+                    element.textContent = currentPlayer.symbol;
+                    Game.turn(event.target.getAttribute('data-index'));
+                }
             }
         }
         
@@ -97,32 +136,64 @@
         };
     })();
 
-    // Player factory function
-    let player = function(symbol, id) {
-
-        return {symbol, id}
-    };
 
     // Controls the game, win conditions
     const Game = (function() {
+        // Player factory function
+        let player = function(symbol, id) {
+            return {symbol, id}
+        };
+
         let players = [];
         let currentPlayer;
+        let gameOverFlag = false;
+        let turnNumber = 1;
 
-        function checkForGameOver(index) {
-            // Check for win vertically:
-            // Get all spaces in column from GameBoard
-            // Check if all spaces in column have same move -> if so, current player wins
+        function checkForGameOver(index, currentSymbol) {
             let column = GameBoard.getColumnByIndex(index);
-            console.log(column);
-
-            // Check for win horizontally: 
-            // Get all spaces in row from GameBoard
-            // Check if all spaces in row have same move -> if so, current player wins
             let row = GameBoard.getRowByIndex(index);
-            console.log(row);
+            let diagonal1, diagonal2;
+
+            console.log({currentSymbol})
+
+            // Check column for win
+            if(column.every(space => space.move === currentSymbol)) {
+                return true;
+            }
+
+            // Check row for win
+            if(row.every(space => space.move === currentSymbol)) {
+                return true;
+            }
+
+            // Check diagonal for win, if corner or middle space
+            if(index % 2 === 0) {
+                if(GameBoard.getSpace(index).row === 'center' && GameBoard.getSpace(index).column === 'middle') {
+                    diagonal1 = GameBoard.getDiagonalCenter(1);
+                    diagonal2 = GameBoard.getDiagonalCenter(2);
+    
+                    if(diagonal1.every(space => space.move === currentSymbol)) {
+                        return true;
+                    } else if(diagonal2.every(space => space.move === currentSymbol)) {
+                        return true;
+                    }
+    
+                } else {
+                    diagonal1 = GameBoard.getDiagonalByIndex(index);
+    
+                    if(diagonal1.every(space => space.move === currentSymbol)) {
+                        return true;
+                    }
+                }
+            }
 
             // Check for tie
-            // If all spaces filled and no one has won, game is a tie.
+            if(turnNumber >= 9) {
+                console.log("tie");
+                return true;
+            }
+
+            return false;
         }
 
         return {
@@ -139,18 +210,27 @@
                 console.log(currentPlayer);
             },
             getCurrentPlayer: function() {
-                return currentPlayer
+                return currentPlayer;
+            },
+            getGameOverFlag: function() {
+                return gameOverFlag;
             },
             turn: function(index) {
                 // Update GameBoard
                 GameBoard.updateSpace(index, currentPlayer.symbol);
-                checkForGameOver(index);
+                gameOverFlag = checkForGameOver(index, currentPlayer.symbol);
 
-                // Switch active player
-                if (currentPlayer.id === 0) {
-                    currentPlayer = players[1];
-                } else {
-                    currentPlayer = players[0];
+                console.log(gameOverFlag);
+
+                if(!gameOverFlag) {
+                    turnNumber++;
+                    console.log(turnNumber);
+                    // Switch active player
+                    if (currentPlayer.id === 0) {
+                        currentPlayer = players[1];
+                    } else {
+                        currentPlayer = players[0];
+                    }
                 }
             },
         }
